4cc58060b4b4abb01cec6ab852bd02f6b49d59bd
Anshu-79 <tanwaranshumaan@gmail.com>
2024-08-27 01:26:23 +0530

    complete login screen frontend

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,19 @@ class MyApp extends StatelessWidget {
 
   final SharedPreferences prefs;
 
+  static ColorScheme colorScheme = ColorScheme(
+      primary: const Color(0xFFEFF3F3),
+      onPrimary: Colors.black87,
+      secondary: Colors.black87,
+      onSecondary: const Color(0xFFEFF3F3),
+      error: Colors.redAccent,
+      onError: Colors.white,
+      surface: const Color(0xFFFAFBFB),
+      onSurface: Colors.black87,
+      brightness: Brightness.light,
+      outline: Colors.blue,
+      tertiary: Colors.blueAccent.shade200);
+
   @override
   Widget build(BuildContext context) {
     bool onboard = prefs.getBool('onboard') ?? false;
@@ -28,11 +41,15 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'Jost',
-        textTheme: TextTheme(),
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue.shade600),
+        textTheme: const TextTheme(),
+        colorScheme: colorScheme,
+        canvasColor: colorScheme.surface,
+        scaffoldBackgroundColor: colorScheme.surface,
+        highlightColor: Colors.transparent,
+        focusColor: colorScheme.primary,
         useMaterial3: true,
       ),
-      home: onboard ? HomeScreen() : LoginScreen(prefs: prefs),
+      home: onboard ? const HomeScreen() : LoginScreen(prefs: prefs),
     );
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -3,7 +3,9 @@ import 'dart:ui';
 import 'package:animated_toggle_switch/animated_toggle_switch.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:blur/blur.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 TextStyle loginPageHeader = const TextStyle(
@@ -11,6 +13,24 @@ TextStyle loginPageHeader = const TextStyle(
     fontSize: 100,
     color: Colors.white,
     fontWeight: FontWeight.bold);
+
+Future<dynamic> signInWithGoogle() async {
+  try {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  } on Exception catch (e) {
+    debugPrint('exception->$e');
+  }
+}
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key, required this.prefs});
@@ -28,17 +48,16 @@ class _LoginScreenState extends State<LoginScreen> {
       textDirection: TextDirection.rtl,
       current: isAdmin,
       values: const [false, true],
-      iconBuilder: (value) =>
-          value ? const Text("Sign Up") : const Text("Login"),
-      indicatorSize: const Size.fromWidth(100),
+      iconBuilder: (value) => value
+          ? const Text("Sign Up", style: TextStyle(fontWeight: FontWeight.w500))
+          : const Text("Admin Login",
+              style: TextStyle(fontWeight: FontWeight.w500)),
+      indicatorSize: const Size.fromWidth(150),
       borderWidth: 4.0,
       iconAnimationType: AnimationType.onHover,
       style: ToggleStyle(
-        borderColor: Colors.transparent,
-        borderRadius: BorderRadius.circular(30.0),
-      ),
-      styleBuilder: (i) =>
-          ToggleStyle(indicatorColor: Theme.of(context).primaryColor),
+          indicatorColor: Theme.of(context).colorScheme.tertiary,
+          borderColor: Colors.white),
       onChanged: (i) => setState(() => isAdmin = i),
     );
   }
@@ -50,24 +69,34 @@ class _LoginScreenState extends State<LoginScreen> {
         children: [
           const LoginBackground(),
           Container(
-            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 10),
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 35),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 AutoSizeText("URJOTSAV", style: loginPageHeader, maxLines: 1),
+                const SizedBox(height: 80),
                 Container(
+                  height: MediaQuery.of(context).size.height / 2,
                   padding: const EdgeInsets.all(8),
                   decoration: BoxDecoration(
                     border: Border.all(
                         color: Colors.white.withOpacity(0.5), width: 5),
-                    borderRadius: BorderRadius.circular(30),
+                    borderRadius: BorderRadius.circular(40),
                   ),
-                  child: Column(
-                    children: [
-                      loginToggle(),
-                    ],
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 20, horizontal: 5),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        loginToggle(),
+                        isAdmin ? const GoogleSignUp() : const AdminLogin(),
+                        const SizedBox()
+                      ],
+                    ),
                   ),
-                ).frosted(borderRadius: BorderRadius.circular(30))
+                ).frosted(
+                    borderRadius: BorderRadius.circular(40),
+                    frostColor: Colors.black12),
               ],
             ),
           ),
@@ -88,5 +117,153 @@ class LoginBackground extends StatelessWidget {
           image: AssetImage('assets/images/login_bg.jpg'),
           fit: BoxFit.fitHeight),
     )).blurred(colorOpacity: 0.1);
+  }
+}
+
+class AdminLogin extends StatefulWidget {
+  const AdminLogin({super.key});
+
+  @override
+  State<AdminLogin> createState() => _AdminLoginState();
+}
+
+class _AdminLoginState extends State<AdminLogin> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            TextFormField(
+              style: const TextStyle(color: Colors.white),
+              controller: emailController,
+              decoration: InputDecoration(
+                labelStyle: const TextStyle(color: Colors.white),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide:
+                          const BorderSide(color: Colors.white, width: 2)),
+                  border: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Colors.white, width: 2),
+                      borderRadius: BorderRadius.circular(30)),
+                  labelText: "Email"),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your email';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: passwordController,
+              obscureText: true,
+              style: const TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                labelStyle: const TextStyle(color: Colors.white),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide:
+                          const BorderSide(color: Colors.white, width: 2)),
+                  border: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Colors.white, width: 2),
+                      borderRadius: BorderRadius.circular(30)),
+                  labelText: "Password"),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your password';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 50),
+            ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // Navigate the user to the Home page
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Please fill input')),
+                    );
+                  }
+                },
+                child: const Text('Submit', style: TextStyle(color: Colors.black87, fontSize: 20),)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class GoogleSignUp extends StatefulWidget {
+  const GoogleSignUp({super.key});
+
+  @override
+  State<GoogleSignUp> createState() => _GoogleSignUpState();
+}
+
+class _GoogleSignUpState extends State<GoogleSignUp> {
+  ValueNotifier userCredential = ValueNotifier('');
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+        valueListenable: userCredential,
+        builder: (context, value, child) {
+          return (userCredential.value == '' || userCredential.value == null)
+              ? Center(
+                  child: ElevatedButton(
+                      onPressed: () async {
+                        userCredential.value = await signInWithGoogle();
+                        if (userCredential.value != null) {
+                          debugPrint(userCredential.value.user!.email);
+                        }
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: Row(children: [
+                          Image.asset('assets/images/google_icon.png',
+                              width: 40),
+                          const SizedBox(width: 10),
+                          const AutoSizeText("Sign Up with Google",
+                              style: TextStyle(fontSize: 15, color: Colors.black87), maxLines: 1)
+                        ]),
+                      )),
+                )
+              : Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        clipBehavior: Clip.antiAlias,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border:
+                                Border.all(width: 1.5, color: Colors.black54)),
+                        child: Image.network(
+                            userCredential.value.user!.photoURL.toString()),
+                      ),
+                      const SizedBox(height: 20),
+                      Text(userCredential.value.user!.displayName.toString()),
+                      const SizedBox(height: 20),
+                      Text(userCredential.value.user!.email.toString()),
+                      const SizedBox(height: 30),
+                      ElevatedButton(
+                          onPressed: () async {}, child: const Text('Logout'))
+                    ],
+                  ),
+                );
+        });
   }
 }
